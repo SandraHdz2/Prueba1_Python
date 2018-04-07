e0a73fa737371810d117dd41c7c7003d6d28ad09
SandraHdz2 <38160566+SandraHdz2@users.noreply.github.com>
2018-04-07 13:36:17 -0500

    Primer_python

---
+++ hola.py
@@ -0,0 +1,26 @@
+"""
+#comentario (gato), largo (doble comilla)
+a= "Sandra Hdz"
+b=20
+print ("Welcome to Python")
+print ("Hola",a,"\nTu edad es",b,"años")
+print ("-------------------------------------")
+print ("Welcome to Python")
+print ("Ingrese su nombre:")
+nombre=input()#datos de entrada
+print ("Ingrese su edad")
+age=input()
+print ("Hola",nombre,"\nTu edad es",age,"años")
+"""
+nombre= input("Nombre:")
+edad=int (input("Edad:"))
+facultad= input("Institucion de estudio:")
+semestre=int (input("Semestre que cursa:"))
+promedio=float (input("Promedio:"))
+turno= input("Turno:")
+print("Hola",nombre,"tu edad es",edad,"\nEstudias en",facultad
+      ,"Semestre que cursas",semestre,"Tu promedio es:",promedio,"Turno:",turno)
+
+
+
+
