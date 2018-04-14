47acb69e5447f2b7224bc9542856db543354f1e3
SandraHdz2 <38160566+SandraHdz2@users.noreply.github.com>
2018-04-14 09:56:22 -0500

    nombre_edad

---
+++ rep.py
@@ -0,0 +1,9 @@
+#concatenar es unir los elementos c;
+creador="Sandra"
+print ("Saludos cordiales te envía",creador)
+nombre=input("Ingresa tu nombre")
+edad=int (input("Ingresa tu edad:"))
+print ("Tu nombre es",nombre,"tu edad es",edad,"años")
+          
+print("El año siguiente tu edad sera:",edad+1)    
+        
